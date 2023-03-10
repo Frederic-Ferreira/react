fd6059bc30690c3ad8f017c2d3a12d2670ac28c6
frederic <fred.69780@hotmail.fr>
2023-03-10 11:41:13 +0100

    modal delete added

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,10 +1,11 @@
-export function TodoItem({ item, handleClick }) {
+export function TodoItem({ item, handleToggleState, handleDelete }) {
 	return (
 		<li>
 			<span>{item.name}</span>
-			<button type="button" onClick={() => handleClick(item)}>
+			<button type="button" onClick={() => handleToggleState(item)}>
 				TODO
 			</button>
+			<button style={{background: 'red', color: 'white'}} type="button" onClick={() => handleDelete()}>DELETE</button>
 		</li>
 	);
 }
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -12,14 +12,12 @@ function App() {
 	const [form, setForm] = useState({
 		todo: '',
 	});
+	const [currentDeleteItem, setCurrentDeleteItem] = useState(null)
+
 	const input = useRef()
+	const modal = useRef()
 
 	const [todos, setTodos] = useState([]);
-
-	function handleClickColor(){
-		console.log('button clicked')
-		input.current.style.background = 'black'
-	}
 
 	function handleSubmit(event) {
 		event.preventDefault();
@@ -45,7 +43,7 @@ function App() {
 		});
 	}
 
-	function handleClick(item) {
+	function handleToggleState(item) {
 		const newTodos = todos.map(function (elem) {
 			if (elem.name !== item.name) return elem;
 
@@ -58,11 +56,31 @@ function App() {
 		setTodos(newTodos);
 	}
 
+	function handleToggleModal(item){
+		setCurrentDeleteItem(item)
+		modal.current.classList.toggle("hidden");
+	}
+
+
+	function handleDelete() {
+		const newTodos = todos.filter(function (elem) {
+			if (elem.name !== currentDeleteItem.name) return elem;
+		});
+		setTodos(newTodos);
+		handleToggleModal(currentDeleteItem)
+	}
+
 	return (
 		<div className="App">
+			<div ref={modal} className="modal hidden">
+				<div className='confirm-delete'>
+					<h1>Do you want to delete this todo ?</h1>
+					<button onClick={() => handleDelete()} type='button'>YES</button>
+					<button onClick={() => handleToggleModal()} type='button'>NO</button>
+				</div>
+			</div>
 			<form onSubmit={handleSubmit}>
 				<input type="text" name="todo" onChange={handleChange} ref={input} value={form.todo} />
-				<button onClick={handleClickColor}>Change color</button>
 				<button type="submit">Ajouter</button>
 			</form>
 			<h2>TODOS</h2>
@@ -72,7 +90,7 @@ function App() {
 						return item.state === STATE.TODO;
 					})
 					.map(function (item) {
-						return <TodoItem key={item.name} item={item} handleClick={handleClick} />;
+						return <TodoItem key={item.name} item={item} handleDelete={() => handleToggleModal(item)} handleToggleState={handleToggleState} />;
 					})}
 			</ul>
 
@@ -83,7 +101,7 @@ function App() {
 						return item.state === STATE.DONE;
 					})
 					.map(function (item) {
-						return <TodoItem key={item.name} item={item} handleClick={handleClick} />;
+						return <TodoItem key={item.name} item={item} handleDelete={() => handleToggleModal(item)} handleToggleState={handleToggleState} />;
 					})}
 			</ul>
 		</div>
